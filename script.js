51b477970bc5522c7c43fc4199086eb35f2c68b2
Felipe Melo <72765665+Imaginisk@users.noreply.github.com>
2022-09-22 23:59:15 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+ Se mouse passar por cima
+ Faça a propriedada imagem surpreso ser executada
+ Se mouse clicar faça a propriedade sorrindo ser executada
+ Senao
+ Faça a propriedade normal ser exercudada
+ Fimse
+
+*/
+/*
+-Pegar botões e fazer eles animarem como se fosse uma lua
+-Fazer botões aumentam quando tocados
+-Animar títulos 
+*/
+function funcaoNormal() {
+  const elem = (document.getElementById("personagem").src =
+    "imagens/Sprite-0001.gif");
+}
+function funcaoSurpreso() {
+  const elem = (document.getElementById("personagem").src =
+    "imagens/SpriteSurpreso.gif");
+}
+function funcaoSorrir() {
+  const elem = (document.getElementById("personagem").src =
+    "imagens/SpriteSorrindo.gif");
+}
